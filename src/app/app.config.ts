7bc62d67cec69cca9b8afd30afb31b926331b17f
Quynh Nguyen <mr.quynh@gmail.com>
2020-04-01 17:09:03 +0700

    (restructure): related service

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -89,7 +89,7 @@ export class AppConfig {
 		required: [],
 		hidden: ["Gender", "BirthDay", "Address", "Addresses", "Mobile"],
 		sendInvitationRole: "Authenticated",
-		setServicePrivilegs: true,
+		setServicePrivilegs: false,
 		setServicePrivilegsRole: "ServiceAdministrator",
 		setObjectPrivilegs: true
 	};
@@ -254,11 +254,9 @@ export class AppConfig {
 	public static getRelatedJson(service?: string, additional?: { [key: string]: string }) {
 		const json: { [key: string]: string } = {
 			"language": this.language,
-			"host": this.url.host
+			"host": this.url.host,
+			"related-service": (AppUtility.isNotEmpty(service) ? service : this.services.active).trim().toLowerCase()
 		};
-		if (AppUtility.isNotEmpty(service) && !AppUtility.isEquals(service, this.services.active)) {
-			json["related-service"] = service.trim().toLowerCase();
-		}
 		if (AppUtility.isObject(additional, true)) {
 			Object.keys(additional).forEach(key => json[key] = additional[key]);
 		}
