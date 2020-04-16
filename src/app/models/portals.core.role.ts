bad592fb945134eff336543c0f5c1ac91749af75
Quynh Nguyen <mr.quynh@gmail.com>
2020-04-16 13:24:23 +0700

    (improv): add properties of nested object

--- src/app/models/portals.core.role.ts
+++ src/app/models/portals.core.role.ts
@@ -1,8 +1,9 @@
 import { Dictionary } from "typescript-collections";
 import { AppUtility } from "../components/app.utility";
+import { PortalNested as NestedModel } from "./portals.base";
 import { PortalCoreBase as BaseModel } from "./portals.core.base";
 
-export class Role extends BaseModel {
+export class Role extends BaseModel implements NestedModel {
 
 	constructor() {
 		super();
@@ -13,9 +14,15 @@ export class Role extends BaseModel {
 	/** All instances of role */
 	public static instances = new Dictionary<string, Role>();
 
+	/** All instances of role */
+	public static get all() {
+		return this.instances.values();
+	}
+
 	ID = "";
 	Title = "";
 	Description = "";
+	ParentID = "";
 	UserIDs = new Array<string>();
 	SystemID = "";
 	Created = new Date();
@@ -23,6 +30,17 @@ export class Role extends BaseModel {
 	LastModified = new Date();
 	LastModifiedID = "";
 	ansiTitle = "";
+
+	public static filter(parentID?: string, organizationID?: string) {
+		let roles = Role.all;
+		if (AppUtility.isNotEmpty(parentID)) {
+			roles = roles.filter(role => role.ParentID === parentID);
+		}
+		else if (AppUtility.isNotEmpty(organizationID)) {
+			roles = roles.filter(role => role.SystemID === organizationID);
+		}
+		return roles.sort(AppUtility.getCompareFunction("Title"));
+	}
 
 	/** Deserializes data to object */
 	public static deserialize(json: any, role?: Role) {
@@ -55,6 +73,23 @@ export class Role extends BaseModel {
 	/** Checks to see the dictionary is contains the object by identity or not */
 	public static contains(id: string) {
 		return id !== undefined && this.instances.containsKey(id);
+	}
+
+	get FullTitle() {
+		const parent = this.Parent;
+		return (parent !== undefined ? `${parent.FullTitle} > ` : "") + this.Title;
+	}
+
+	get OrderIndex() {
+		return 0;
+	}
+
+	get Parent() {
+		return AppUtility.isNotEmpty(this.ParentID) ? Role.get(this.ParentID) : undefined;
+	}
+
+	get Children() {
+		return Role.filter(this.ID);
 	}
 
 }
