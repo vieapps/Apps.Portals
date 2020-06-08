652179043e6d75b46d7e121ef269634336789df0
Quynh Nguyen <mr.quynh@gmail.com>
2020-06-09 02:23:39 +0700

    (improv): normalize rich html

--- src/app/services/portals.cms.service.ts
+++ src/app/services/portals.cms.service.ts
@@ -39,7 +39,7 @@ export class PortalsCmsService extends BaseService {
 		this.initializeAsync();
 	}
 
-	private oembedProviders: Array<{ name: string; urlPatterns: RegExp[], html: string; }>;
+	private oembedProviders: Array<{ name: string; urlPatterns: RegExp[], idPattern: { regex: RegExp; position: number }, html: string; }>;
 
 	private initialize() {
 		AppRTU.registerAsObjectScopeProcessor(this.name, "Category", message => this.processCategoryUpdateMessage(message));
@@ -59,11 +59,15 @@ export class PortalsCmsService extends BaseService {
 
 	public async initializeAsync() {
 		if (this.oembedProviders === undefined) {
-			const oembedProviders = await AppXHR.makeRequest("GET", "/assets/js/oembed.providers.json").toPromise() as Array<{ name: string; schemes: string[], html: string; }>;
+			const oembedProviders = await AppXHR.makeRequest("GET", this.configSvc.appConfig.URIs.apis + "statics/oembed.providers.json").toPromise() as Array<any>;
 			this.oembedProviders = oembedProviders.map(provider => {
 				return {
 					name: provider.name,
-					urlPatterns: provider.schemes.map(scheme => new RegExp(scheme.replace(/\*/g, "(.*)"), "i")),
+					urlPatterns: (provider.schemes as Array<string>).map(scheme => new RegExp(scheme.replace(/\*/g, "(.*)"), "i")),
+					idPattern: {
+						regex: AppUtility.toRegExp(provider.pattern.app.regex),
+						position: provider.pattern.app.position as number
+					},
 					html: provider.html
 				};
 			});
@@ -98,25 +102,23 @@ export class PortalsCmsService extends BaseService {
 			// normalize all 'oembed' tags
 			let start = AppUtility.indexOf(html, "<oembed");
 			while (start > -1) {
-				const end = start < 0 ? -1 : AppUtility.indexOf(html, "</oembed>", start + 1);
+				let end = start < 0 ? -1 : AppUtility.indexOf(html, "</oembed>", start + 1);
 				if (end > -1) {
+					end += 9;
 					let media = (html as string).substr(start, 9 + end - start);
 					const urlStart = AppUtility.indexOf(media, "url=") + 5;
 					const urlEnd = AppUtility.indexOf(media, "\"", urlStart + 1);
 					const url = media.substr(urlStart, urlEnd - urlStart);
 					const oembedProvider = this.oembedProviders.find(provider => provider.urlPatterns.some(pattern => url.match(pattern)));
 					if (oembedProvider !== undefined) {
-						let mediaID: string;
-						if (AppUtility.isEquals(oembedProvider.name, "YouTube")) {
-							const match = url.match(/^.*(youtu.be\/|youtube(-nocookie)?.com\/(v\/|.*u\/\w\/|embed\/|.*v=))([\w-]{11}).*/);
-							mediaID = match !== undefined && match.length > 4 ? match[4] : undefined;
-							media = AppUtility.format(oembedProvider.html, { id: mediaID });
-						}
+						const match = url.match(oembedProvider.idPattern.regex);
+						const mediaID = AppUtility.isArray(match, true) && match.length > oembedProvider.idPattern.position ? match[oembedProvider.idPattern.position] : undefined;
+						media = AppUtility.format(oembedProvider.html, { id: mediaID });
 					}
 					else {
 						media = url.endsWith(".mp3")
-							? AppUtility.format("<audio controls=\"true\" width=\"560\" height=\"32\" src=\"{{url}}\"></audio>", { url: url })
-							: AppUtility.format("<video controls=\"true\" width=\"560\" height=\"315\" src=\"{{url}}\"></video>", { url: url });
+							? AppUtility.format("<audio width=\"560\" height=\"32\" controls autoplay muted><source src=\"{{url}}\"/></audio>", { url: url })
+							: AppUtility.format("<video width=\"560\" height=\"315\" controls autoplay muted><source src=\"{{url}}\"/></video>", { url: url });
 					}
 					html = html.substr(0, start) + media + html.substr(end);
 				}
