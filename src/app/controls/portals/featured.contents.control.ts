30f9d39454e183191368ea463eba879f6d80370d
Quynh Nguyen <mr.quynh@gmail.com>
2021-01-07 02:44:06 +0700

    (improv): normalize featured contents

--- src/app/controls/portals/featured.contents.control.ts
+++ src/app/controls/portals/featured.contents.control.ts
@@ -2,10 +2,11 @@ import { Component, OnInit, OnDestroy, Input } from "@angular/core";
 import { AppEvents } from "@app/components/app.events";
 import { AppUtility } from "@app/components/app.utility";
 import { PlatformUtility } from "@app/components/app.utility.platform";
-import { PortalCmsBase as CmsBaseModel } from "@app/models/portals.cms.base";
 import { ConfigurationService } from "@app/services/configuration.service";
 import { PortalsCoreService } from "@app/services/portals.core.service";
 import { PortalsCmsService } from "@app/services/portals.cms.service";
+import { PortalCmsBase as CmsBaseModel } from "@app/models/portals.cms.base";
+import { Category } from "@app/models/portals.cms.category";
 
 @Component({
 	selector: "control-cms-portals-featured-contents",
@@ -28,7 +29,7 @@ export class FeaturedContentsControl implements OnInit, OnDestroy {
 	@Input() orderBy: string;
 	@Input() amount: number;
 
-	contents = new Array<CmsBaseModel>();
+	contents = new Array<FeaturedContent>();
 	private _isPublished = false;
 	private _preparing = false;
 
@@ -114,31 +115,57 @@ export class FeaturedContentsControl implements OnInit, OnDestroy {
 	private prepareContents(force: boolean = false) {
 		if (!this.contents.length || force) {
 			const organizationID = this.portalsCoreSvc.activeOrganization !== undefined ? this.portalsCoreSvc.activeOrganization.ID : undefined;
-			const filterBy: (content: CmsBaseModel) => boolean = AppUtility.isNotEmpty(this.status)
-				? content => content.SystemID === organizationID && content.Status === this.status
-				: content => content.SystemID === organizationID;
-			const orderBy = [{ name: "LastModified", reverse: true }];
-			if (this._isPublished) {
-				orderBy.insert({ name: "StartDate", reverse: true }, 0);
-				orderBy.insert({ name: "PublishedTime", reverse: true }, 1);
-			}
-			this.contents = this.portalsCmsSvc.featuredContents.filter(filterBy).orderBy(orderBy).take(this.amount);
+			const filterBy: (content: FeaturedContent) => boolean = AppUtility.isNotEmpty(this.status)
+				? content => content.OriginalObject.SystemID === organizationID && content.Status === this.status
+				: content => content.OriginalObject.SystemID === organizationID;
+			const orderBy = this._isPublished
+				? [{ name: "StartDate", reverse: true }, { name: "PublishedTime", reverse: true }]
+				: [];
+			orderBy.push({ name: "LastModified", reverse: true });
+			this.contents = this.portalsCmsSvc.featuredContents.map(content => {
+				const category = content["category"];
+				return {
+					ID: content.ID,
+					Title: content.Title,
+					Created: content.Created,
+					LastModified: content.LastModified,
+					PublishedTime: content["PublishedTime"] || content.Created,
+					Status: content.Status,
+					ThumbnailURI: content.thumbnailURI,
+					StartDate: content["StartDate"] || content.Created,
+					Category: category !== undefined && typeof category === "object" ? category : undefined,
+					OriginalObject: content
+				} as FeaturedContent;
+			}).filter(filterBy).orderBy(orderBy).take(this.amount);
 		}
 	}
 
-	track(index: number, content: CmsBaseModel) {
+	track(index: number, content: FeaturedContent) {
 		return `${content.ID}@${index}`;
 	}
 
-	async viewAsync(event: any, object: CmsBaseModel) {
+	async viewAsync(event: any, object: FeaturedContent) {
 		event.stopPropagation();
-		if (this.portalsCoreSvc.activeOrganization === undefined || this.portalsCoreSvc.activeOrganization.ID !== object.SystemID) {
-			await this.portalsCoreSvc.getActiveOrganizationAsync(object.SystemID);
+		if (this.portalsCoreSvc.activeOrganization === undefined || this.portalsCoreSvc.activeOrganization.ID !== object.OriginalObject.SystemID) {
+			await this.portalsCoreSvc.getActiveOrganizationAsync(object.OriginalObject.SystemID);
 		}
-		if (this.portalsCoreSvc.activeModule === undefined || this.portalsCoreSvc.activeModule.ID !== object.RepositoryID) {
-			await this.portalsCoreSvc.getActiveModuleAsync(object.RepositoryID);
+		if (this.portalsCoreSvc.activeModule === undefined || this.portalsCoreSvc.activeModule.ID !== object.OriginalObject.RepositoryID) {
+			await this.portalsCoreSvc.getActiveModuleAsync(object.OriginalObject.RepositoryID);
 		}
-		await this.configSvc.navigateForwardAsync(this.portalsCoreSvc.getAppURL(object.contentType, "view", object["category"] ? object["category"].Title : undefined, { ID: object.ID }));
+		await this.configSvc.navigateForwardAsync(this.portalsCoreSvc.getAppURL(object.OriginalObject.contentType, "view", object.Category ? object.Category.Title : undefined, { ID: object.ID }));
 	}
 
+}
+
+export interface FeaturedContent {
+	ID: string;
+	Title: string;
+	Status: string;
+	ThumbnailURI: string;
+	Created: Date;
+	LastModified: Date;
+	PublishedTime: Date;
+	StartDate?: string;
+	Category?: Category;
+	OriginalObject: CmsBaseModel;
 }
