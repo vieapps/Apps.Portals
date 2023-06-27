54d53c1c459a2f3a581a445a6f8325ae6b2deddf
Quynh Nguyen <mr.quynh@gmail.com>
2023-06-27 16:04:47 +0700

    (improv): fetch all organizations

--- src/app/services/portals.core.service.ts
+++ src/app/services/portals.core.service.ts
@@ -106,6 +106,10 @@ export class PortalsCoreService extends BaseService {
 		return 0;
 	}
 
+	get allowSelectActiveOrganization() {
+		return true;
+	}
+
 	initialize() {
 		AppAPIs.registerAsServiceScopeProcessor(this.name, message => {
 			if (message.Data !== undefined) {
@@ -232,6 +236,9 @@ export class PortalsCoreService extends BaseService {
 			if (Organization.active === undefined) {
 				this.prepareSidebar();
 			}
+		}
+		if (this.allowSelectActiveOrganization) {
+			AppUtility.invoke(() => this.fetchOrganizationsAsync(), 789);
 		}
 		await this.prepareSidebarFooterItemsAsync();
 		this.activeSidebar(onNext);
@@ -1634,13 +1641,11 @@ export class PortalsCoreService extends BaseService {
 		return controls;
 	}
 
-	prepareApprovalStatusControl(controlConfig: AppFormsControlConfig, selectInterface?: string) {
-		this.appFormsSvc.prepareSelectControl(controlConfig, controlConfig.Options.SelectOptions.Values, _ => {
-			controlConfig.Options.SelectOptions.Interface = selectInterface || "popover";
-			controlConfig.Options.SelectOptions.Values = AppUtility.isGotData(controlConfig.Options.SelectOptions.Values)
-				? (controlConfig.Options.SelectOptions.Values as AppFormsLookupValue[]).map(kvp => ({ Value: kvp.Value, Label: `{{status.approval.${kvp.Value}}}` }) as AppFormsLookupValue)
-				: BaseModel.approvalStatus.map(value => ({ Value: value, Label: `{{status.approval.${value}}}` }) as AppFormsLookupValue);
-		});
+	prepareApprovalStatusControl(controlConfig: AppFormsControlConfig) {
+		this.appFormsSvc.prepareSelectControl(controlConfig, controlConfig.Options.SelectOptions.Values, _ => controlConfig.Options.SelectOptions.Values = AppUtility.isGotData(controlConfig.Options.SelectOptions.Values)
+			? (controlConfig.Options.SelectOptions.Values as AppFormsLookupValue[]).map(kvp => ({ Value: kvp.Value, Label: `{{status.approval.${kvp.Value}}}` }) as AppFormsLookupValue)
+			: BaseModel.approvalStatus.map(value => ({ Value: value, Label: `{{status.approval.${value}}}` }) as AppFormsLookupValue)
+		);
 		return controlConfig;
 	}
 
@@ -2056,12 +2061,35 @@ export class PortalsCoreService extends BaseService {
 		);
 	}
 
-	searchOrganizationsAsync(request: AppDataRequest, onSuccess?: (data?: any) => void, onError?: (error?: any) => void) {
+	searchOrganizationsAsync(request: AppDataRequest, onSuccess?: (data?: any) => void, onError?: (error?: any) => void, relatedQuery?: string) {
 		return this.searchAsync(
-			this.getSearchingPath("organization", this.configSvc.relatedQuery),
+			this.getSearchingPath("organization", relatedQuery || this.configSvc.relatedQuery),
 			request,
 			data => this.processOrganizations(data, onSuccess),
 			error => this.processError("Error occurred while searching organizations", error, onError)
+		);
+	}
+
+	private fetchOrganizationsAsync(pagination?: AppDataPagination) {
+		return this.searchAsync(
+			this.getSearchingPath("organization", this.configSvc.relatedQuery + "&x-fetch=ngx-apps"),
+			AppPagination.buildRequest(undefined, undefined, pagination),
+			data => {
+				const ids = new Array<string>();
+				if (data !== undefined && AppUtility.isGotData(data.Objects)) {
+					(data.Objects as Array<any>).filter(org => !Organization.contains(org.ID)).forEach(org => {
+						Organization.set(Organization.deserialize(org));
+						ids.push(org.ID);
+					});
+				}
+				const pagination = AppPagination.getDefault(data);
+				if (pagination.PageNumber < pagination.TotalPages) {
+					AppUtility.invoke(() => this.fetchOrganizationsAsync(pagination), 123);
+				}
+				AppUtility.invoke(() => ids.forEach(id => this.getOrganizationAsync(id)), 456);
+			},
+			error => this.processError("Error occurred while fetching organizations", error),
+			true
 		);
 	}
 
